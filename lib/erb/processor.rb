9f48628d8397f4bd233eddd4aa1221a9a2cf6c18
Christophe Broult <cbroult@yahoo.com>
2022-06-22 13:49:35 +0200

    Boilerplate Ruby gem code tree structure w/ no change

---
+++ lib/erb/processor.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "processor/version"
+
+module Erb
+  module Processor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/erb/processor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Erb::Processor do
+  it "has a version number" do
+    expect(Erb::Processor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
